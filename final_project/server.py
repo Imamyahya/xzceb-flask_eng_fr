7e9cb03d23c229144faa4e5cb99f9e147640232f
Imamyahya <yahyaharuna1006@gmail.com>
2023-06-21 07:05:17 -0400

    commit on master

--- final_project/server.py
+++ final_project/server.py
@@ -19,7 +19,7 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    return render_template('index.html') #gggggggg
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
